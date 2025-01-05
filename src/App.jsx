8290550d52a4c5f0fb8d0480dfc33eb0278d6a56
Arthur Smielewski <arthur.smielewski@gmail.com>
2025-01-05 23:13:43 +0000

    react update + some work
    - updated react to v19
    - installed tanstack query
    - replaced fetch using use hook with tanstack query
    - styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import "./App.css"
-import { Data } from "./Data"
-import { Suspense } from "react"
-import { ErrorBoundary } from "react-error-boundary"
+import RecipeGrid from './RecipeGrid';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const apiUrl = 'https://production-api.gousto.co.uk/cmsreadbroker/v1/categories?limit=4&offset=0&recipesCount=4'
+const queryClient = new QueryClient();
 
 function App() {
 
   return (
     <>
-    <ErrorBoundary fallback = {<div>Error!</div>}>
-      <Suspense fallback = {<div>Loading...</div>}>
-        <Data url = {apiUrl}/>
-      </Suspense>
-    </ErrorBoundary>
+      <QueryClientProvider client = {queryClient}>
+        <RecipeGrid/>
+      </QueryClientProvider>
     </>
   )
 }
---
+++ src/RecipeGrid.jsx
@@ -0,0 +1,66 @@
+import "./RecipeGrid.css"
+import { useQuery } from '@tanstack/react-query';
+
+const fetchAllRecipes = async () => {
+  const res = await fetch('https://6774130577a26d4701c72ab8.mockapi.io/api/v1/recipes/');
+  
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message || `HTTP error, status: ${res.status}`);
+  }
+
+  return res.json();
+};
+
+function RecipeComponent({ recipe }) {
+  const prepTime = recipe.prep_time;
+  let displayTime;
+
+  if (prepTime < 61) {
+    displayTime = `${prepTime} mins`;
+  } else {
+    const hours = Math.floor(prepTime / 60);
+    const mins = prepTime % 60;
+    displayTime = `${hours} h ${mins} mins`;
+  }
+
+
+  return (
+    <div className="recipe-card">
+      <img src={recipe.image} alt={recipe.name}/>
+      <button>Add</button>
+      <p className='prep-time'>{displayTime}</p>
+      <h3>{recipe.name}</h3>      
+    </div>
+  );
+}
+
+function RecipeGrid() {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['apiResponse'],
+    queryFn: fetchAllRecipes,
+  });
+
+  if (isLoading) return (
+    <div className="loading-state">
+      <div className="loader"></div>
+      <p>Loading recipes...</p>
+    </div>
+  );
+  if (isError) return (
+    <div className='error-state'>
+      <h3>Error:</h3>
+      <p>{error.message}</p>
+    </div>
+  );
+
+  return (
+    <div className="recipes-container">
+      {data.map((recipe) => (
+        <RecipeComponent key={recipe.id} recipe={recipe} />
+      ))}
+    </div>
+  );
+}
+
+export default RecipeGrid;
